ef29ce7ca4d5e7789683580a95e54f47605ec755
neckhurts <kaizentheplaya@proton.me>
2026-03-11 21:38:42 +0000

    kernel/reboot: Change KernelSU-Next to KernelSU-legacy with hook fixes

--- ksu.sh
+++ ksu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 [ ! -e "KernelSU/kernel/setup.sh" ] && \
-git clone https://github.com/KernelSU-Next/KernelSU-Next KernelSU && 
-cd KernelSU && git branch v1.1.1 && 
+git clone https://github.com/rsuntk/KernelSU KernelSU && 
+cd KernelSU && git branch v3.0.0-30-legacy && 
 cd ../
 
 if [[ $BUILD_SUSFS == 'true' && $BUILD_KERNEL_KSU == 'true' ]]; then
